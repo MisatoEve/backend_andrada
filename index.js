b36fa5761817bf26c67c1e3144c7549d0c2baf83
MisatoEve <eve.andrada3@gmail.com>
2022-11-20 14:15:19 -0300

    ajustes desafío

--- index.js
+++ index.js
@@ -1,28 +1,31 @@
 //▼Primer desafío entregable
 
 //►Creación de clase "ProductManager"
+
 class ProductManager {
 //▼variable privada products
-    #products
+    #products;
 
     constructor() {
         this.#products = [];
     }
+
 //▼Función para devolver los productos que se encuentran guardados
 //►No recibe parámetros
 //►Devuelve el array de productos
     getProducts = () => {
-        return this.#products
+        return this.#products;
     };
+
 //▼Función que genera el id automático de los productos
 //►No recibe parámetros
 //►Devuelve un Id generado a travéz del largo del array de productos
     createProductId = () => {
         const i = this.#products.length;
         const id = i > 0 ? i + 1 : 1;
-
         return id;
     };
+
 //▼Función que buscará la coincidencia entre lo ingresado y lo guardado
 //►Recibe como parámetro el código del nuevo producto
 //►Devolverá Verdadero si lo encontró o Falso si no.
@@ -35,14 +38,14 @@ class ProductManager {
 //▼Función para validar las variables ingresadas cuando se agregan productos
 //►Recibe por parámetro todas las variables del objeto nuevo producto
 //►Devolverá Verdadero si cumple con las validaciones o Falso si no.
-    validation = ({ title, description, price, thumbnail, code, stock }) => {
+    validationInputs = ({ title, description, price, thumbnail, code, stock }) => {
         return (
             title.trim().length > 0 &&
             description.trim().length > 0 &&
             thumbnail.trim().length > 0 &&
             code.trim().length > 0 &&
-            price.toString().trim.length > 0 &&
-            stock.toString().trim.length > 0 &&
+            price.toString().trim().length > 0 &&
+            stock.toString().trim().length > 0 &&
             price > 0 &&
             stock > 0            
         );
@@ -51,7 +54,6 @@ class ProductManager {
 //▼Función encargada de buscar la coincidencia entre el Id ingresado y los productos guardados en el array
 //►Recibe por parámetro un Id
 //►Devolverá el producto si hay coincidencia, de lo contrario mostrará "not found"
-
     getProductsById = (productId) => {
         const copyProducts = [...this.#products];
         const searchProduct = copyProducts.find((product) => product.id === productId);
@@ -74,25 +76,28 @@ class ProductManager {
         };
         if (this.searchProductCode(newProduct.code)) {
             console.error(`(!) Product ${newProduct.code} Registration Failed: Product is already registered`)
+//            console.error(`(!) Error de registro de Producto ${newProduct.code}: el producto ya está registrado`)
             return
         }
-        if (!this.validation({...newProduct})) {
-            console.error(`Please make sure all fields are filled in correctly`)
+        if (!this.validationInputs({...newProduct})) {
+            console.error(`>>Please make sure all fields are filled in correctly<< | >>Por favor, asegúrese de que todos los campos se completen correctamente<<`);
+//            console.error(`>>Por favor, asegúrese de que todos los campos se completen correctamente<<`);
         } else {
             this.#products.push(newProduct);
-            console.log(`The product: ${newProduct.title} has been added successfully`);
+            console.log(`>>The product: '${newProduct.title}' has been added successfully<< | >>El producto: '${newProduct.title}' se ha agregado con éxito<<`);
+//            console.log(`El producto: ${newProduct.title} se ha agregado con éxito`)
         }
     };
 }
 
 //▼Testing por consola
+const misato = new ProductManager();
 
-const misatoEve = new ProductManager();
-
-console.log('Productos registrados en la base de datos:', misatoEve.getProducts());
+console.log('Productos registrados en la base de datos:', misato.getProducts());
 console.log('▼_______________inicia test_______________▼');
 console.log('___________________________________________');
-misatoEve.addProduct(
+console.log('___________________________________________');
+misato.addProduct(
     "producto prueba",
     "Este es un producto prueba",
     200,
@@ -100,30 +105,37 @@ misatoEve.addProduct(
     "abc123",
     25
 );
-console.log('Productos registrados en la base de datos:', misatoEve.getProducts());
+console.log('Productos registrados en la base de datos:', misato.getProducts());
 console.log('▼_________________________________________▼');
 console.log('___________________________________________');
-misatoEve.addProduct(
-    "producto prueba",
+misato.addProduct(
+    "producto prueba2",
     "Este es un producto prueba2",
     300,
     "Sin imagen",
-    "abc123",
+    "abc1234",
     25
 );
-console.log('Productos registrados en la base de datos:', misatoEve.getProducts());
+console.log('Productos registrados en la base de datos:', misato.getProducts());
 console.log('▼_________________________________________▼');
 console.log('___________________________________________');
-misatoEve.addProduct(
-    "producto prueba",
-    "Este es un producto prueba2",
+misato.addProduct(
+    "producto prueba3",
+    "Este es un producto prueba3",
     400,
     "Sin imagen",
-    "abc123",
+    "abcd123",
     20
 );
-console.log('Productos registrados en la base de datos:', misatoEve.getProducts());
-console.log('___________________________________________');
-misatoEve.getProductsById(30);
-misatoEve.getProductsById(2);
-console.log('▲______________finaliza test______________▲') 
+console.log('Productos registrados en la base de datos:', misato.getProducts());
+console.log('▼___________________________________________▼');
+misato.getProductsById(30);
+console.log('▲___________________________________________▲');
+console.log('▼___________________________________________▼');
+misato.getProductsById(1);
+console.log('▲___________________________________________▲');
+console.log('▼___________________________________________▼');
+misato.getProductsById(4);
+console.log('▲___________________________________________▲');
+console.log('____________________________________________');
+console.log('▲_______________finaliza test_______________▲') 
